d52b1dba785a5ce7d9dd44dc51d4ecd05a9306ff
Lucky Eni <realchartwitluckyenison12>
2026-07-04 07:35:58 +0100

    still working

--- contacts-project/routes/index.js
+++ contacts-project/routes/index.js
@@ -1,5 +1,9 @@
 const router = require("express").Router();
 
+router.get("/", (req, res) => {
+  res.send("Welcome to the Contacts API");
+});
+
 router.use("/contacts", require("./contacts"));
 
 module.exports = router;
