51dd27eb3259ef7b47c778abb4d0325b9d01a826
Lucky Eni <realchartwitluckyenison12>
2026-08-09 02:50:05 +0100

    updates

--- library-management-api/server.js
+++ library-management-api/server.js
@@ -4,7 +4,6 @@ import mongoose from "mongoose";
 import cors from "cors";
 import swaggerUi from "swagger-ui-express";
 import swaggerDocument from "./swagger/swagger.json" with { type: "json" };
-
 import booksRoutes from "./routes/booksRoutes.js";
 import authorsRoutes from "./routes/authorsRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
@@ -16,67 +15,43 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
 // DATABASE
-
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
     console.log("Connected to MongoDB");
   })
   .catch((error) => {
-    console.log(
-      "Database Error:",
-      error.message
-    );
+    console.log("Database Error:", error.message);
   });
 
-
 // ROUTES
-
-app.use(
-  "/books",
-  booksRoutes
-);
-
-app.use(
-  "/authors",
-  authorsRoutes
-);
-
-app.use(
-  "/auth",
-  authRoutes
-);
-
+app.use("/books", booksRoutes);
+app.use("/authors", authorsRoutes);
+app.use("/auth", authRoutes);
 
 // SWAGGER
-
 app.use(
   "/api-docs",
   swaggerUi.serve,
   swaggerUi.setup(swaggerDocument)
 );
 
+// HOME ROUTE
+app.get("/", (req, res) => {
+  res.send("Library Management API is running");
+});
 
 // ERROR HANDLER
-
-app.use(
-  (error, req, res, next) => {
-    res.status(500).json({
-      message: error.message
-    });
-  }
-);
-
+app.use((error, req, res, next) => {
+  res.status(500).json({
+    message: error.message
+  });
+});
 
 // SERVER
-
-const PORT =
-  process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(
-    `Server running at http://localhost:${PORT}`
-  );
+  console.log(`Server running at http://localhost:${PORT}`);
 });
